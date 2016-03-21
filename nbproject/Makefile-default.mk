cc51fc377202411eee1c068efea4c4775787cc2e
andrew-jacobs <andrew.jacobs@btinternet.com>
2016-03-21 12:54:21 +0000

    Added missing code files. Builds but untested

---
+++ nbproject/Makefile-default.mk
@@ -0,0 +1,274 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+ifeq "${IGNORE_LOCAL}" "TRUE"
+# do not include local makefile. User is passing all local related variables already
+else
+include Makefile
+# Include makefile containing local settings
+ifeq "$(wildcard nbproject/Makefile-local-default.mk)" "nbproject/Makefile-local-default.mk"
+include nbproject/Makefile-local-default.mk
+endif
+endif
+
+# Environment
+MKDIR=gnumkdir -p
+RM=rm -f 
+MV=mv 
+CP=cp 
+
+# Macros
+CND_CONF=default
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+OUTPUT_SUFFIX=elf
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+else
+IMAGE_TYPE=production
+OUTPUT_SUFFIX=hex
+DEBUGGABLE_SUFFIX=elf
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+endif
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Source Files Quoted if spaced
+SOURCEFILES_QUOTED_IF_SPACED=em-1802.s em-6502.s em-65c02.s em-6800.s em-6809.s em-8080.s em-retro.s em-scmp.s em-z80.s firmware.s
+
+# Object Files Quoted if spaced
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/em-1802.o ${OBJECTDIR}/em-6502.o ${OBJECTDIR}/em-65c02.o ${OBJECTDIR}/em-6800.o ${OBJECTDIR}/em-6809.o ${OBJECTDIR}/em-8080.o ${OBJECTDIR}/em-retro.o ${OBJECTDIR}/em-scmp.o ${OBJECTDIR}/em-z80.o ${OBJECTDIR}/firmware.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/em-1802.o.d ${OBJECTDIR}/em-6502.o.d ${OBJECTDIR}/em-65c02.o.d ${OBJECTDIR}/em-6800.o.d ${OBJECTDIR}/em-6809.o.d ${OBJECTDIR}/em-8080.o.d ${OBJECTDIR}/em-retro.o.d ${OBJECTDIR}/em-scmp.o.d ${OBJECTDIR}/em-z80.o.d ${OBJECTDIR}/firmware.o.d
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/em-1802.o ${OBJECTDIR}/em-6502.o ${OBJECTDIR}/em-65c02.o ${OBJECTDIR}/em-6800.o ${OBJECTDIR}/em-6809.o ${OBJECTDIR}/em-8080.o ${OBJECTDIR}/em-retro.o ${OBJECTDIR}/em-scmp.o ${OBJECTDIR}/em-z80.o ${OBJECTDIR}/firmware.o
+
+# Source Files
+SOURCEFILES=em-1802.s em-6502.s em-65c02.s em-6800.s em-6809.s em-8080.s em-retro.s em-scmp.s em-z80.s firmware.s
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+# fixDeps replaces a bunch of sed/cat/printf statements that slow down the build
+FIXDEPS=fixDeps
+
+.build-conf:  ${BUILD_SUBPROJECTS}
+ifneq ($(INFORMATION_MESSAGE), )
+	@echo $(INFORMATION_MESSAGE)
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+
+MP_PROCESSOR_OPTION=24EP512GP202
+MP_LINKER_FILE_OPTION=,--script=p24EP512GP202.gld
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/em-1802.o: em-1802.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-1802.o.d 
+	@${RM} ${OBJECTDIR}/em-1802.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-1802.s  -o ${OBJECTDIR}/em-1802.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-1802.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-1802.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6502.o: em-6502.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6502.o.d 
+	@${RM} ${OBJECTDIR}/em-6502.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6502.s  -o ${OBJECTDIR}/em-6502.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6502.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6502.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-65c02.o: em-65c02.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-65c02.o.d 
+	@${RM} ${OBJECTDIR}/em-65c02.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-65c02.s  -o ${OBJECTDIR}/em-65c02.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-65c02.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-65c02.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6800.o: em-6800.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6800.o.d 
+	@${RM} ${OBJECTDIR}/em-6800.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6800.s  -o ${OBJECTDIR}/em-6800.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6800.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6800.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6809.o: em-6809.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6809.o.d 
+	@${RM} ${OBJECTDIR}/em-6809.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6809.s  -o ${OBJECTDIR}/em-6809.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6809.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6809.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-8080.o: em-8080.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-8080.o.d 
+	@${RM} ${OBJECTDIR}/em-8080.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-8080.s  -o ${OBJECTDIR}/em-8080.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-8080.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-8080.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-retro.o: em-retro.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-retro.o.d 
+	@${RM} ${OBJECTDIR}/em-retro.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-retro.s  -o ${OBJECTDIR}/em-retro.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-retro.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-retro.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-scmp.o: em-scmp.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-scmp.o.d 
+	@${RM} ${OBJECTDIR}/em-scmp.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-scmp.s  -o ${OBJECTDIR}/em-scmp.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-scmp.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-scmp.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-z80.o: em-z80.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-z80.o.d 
+	@${RM} ${OBJECTDIR}/em-z80.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-z80.s  -o ${OBJECTDIR}/em-z80.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-z80.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-z80.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/firmware.o: firmware.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/firmware.o.d 
+	@${RM} ${OBJECTDIR}/firmware.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  firmware.s  -o ${OBJECTDIR}/firmware.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf   -Wa,-MD,"${OBJECTDIR}/firmware.o.d",--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/firmware.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+else
+${OBJECTDIR}/em-1802.o: em-1802.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-1802.o.d 
+	@${RM} ${OBJECTDIR}/em-1802.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-1802.s  -o ${OBJECTDIR}/em-1802.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-1802.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-1802.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6502.o: em-6502.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6502.o.d 
+	@${RM} ${OBJECTDIR}/em-6502.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6502.s  -o ${OBJECTDIR}/em-6502.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6502.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6502.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-65c02.o: em-65c02.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-65c02.o.d 
+	@${RM} ${OBJECTDIR}/em-65c02.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-65c02.s  -o ${OBJECTDIR}/em-65c02.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-65c02.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-65c02.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6800.o: em-6800.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6800.o.d 
+	@${RM} ${OBJECTDIR}/em-6800.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6800.s  -o ${OBJECTDIR}/em-6800.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6800.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6800.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-6809.o: em-6809.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-6809.o.d 
+	@${RM} ${OBJECTDIR}/em-6809.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-6809.s  -o ${OBJECTDIR}/em-6809.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-6809.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-6809.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-8080.o: em-8080.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-8080.o.d 
+	@${RM} ${OBJECTDIR}/em-8080.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-8080.s  -o ${OBJECTDIR}/em-8080.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-8080.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-8080.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-retro.o: em-retro.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-retro.o.d 
+	@${RM} ${OBJECTDIR}/em-retro.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-retro.s  -o ${OBJECTDIR}/em-retro.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-retro.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-retro.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-scmp.o: em-scmp.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-scmp.o.d 
+	@${RM} ${OBJECTDIR}/em-scmp.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-scmp.s  -o ${OBJECTDIR}/em-scmp.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-scmp.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-scmp.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/em-z80.o: em-z80.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/em-z80.o.d 
+	@${RM} ${OBJECTDIR}/em-z80.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  em-z80.s  -o ${OBJECTDIR}/em-z80.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/em-z80.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/em-z80.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+${OBJECTDIR}/firmware.o: firmware.s  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} "${OBJECTDIR}" 
+	@${RM} ${OBJECTDIR}/firmware.o.d 
+	@${RM} ${OBJECTDIR}/firmware.o 
+	${MP_CC} $(MP_EXTRA_AS_PRE)  firmware.s  -o ${OBJECTDIR}/firmware.o  -c -mcpu=$(MP_PROCESSOR_OPTION)  -omf=elf   -Wa,-MD,"${OBJECTDIR}/firmware.o.d",--defsym=__MPLAB_BUILD=1,-g,--no-relax$(MP_EXTRA_AS_POST)
+	@${FIXDEPS} "${OBJECTDIR}/firmware.o.d"  $(SILENT)  -rsi ${MP_CC_DIR}../  
+	
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemblePreproc
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk    
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -D__DEBUG -D__MPLAB_DEBUGGER_SIMULATOR=1  -omf=elf       -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_SIMULATOR=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
+	
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
+	@${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC} $(MP_EXTRA_LD_PRE)  -o dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX}  ${OBJECTFILES_QUOTED_IF_SPACED}      -mcpu=$(MP_PROCESSOR_OPTION)        -omf=elf   -Wl,--local-stack,--defsym=__MPLAB_BUILD=1,$(MP_LINKER_FILE_OPTION),--stack=16,--check-sections,--data-init,--pack-data,--handles,--isr,--no-gc-sections,--fill-upper=0,--stackguard=16,--no-force-link,--smart-io,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--report-mem$(MP_EXTRA_LD_POST) 
+	${MP_CC_DIR}\\xc16-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/EM-RETRO.${IMAGE_TYPE}.${DEBUGGABLE_SUFFIX} -a  -omf=elf  
+	
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+
+# Subprojects
+.clean-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/default
+	${RM} -r dist/default
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+DEPFILES=$(shell mplabwildcard ${POSSIBLE_DEPFILES})
+ifneq (${DEPFILES},)
+include ${DEPFILES}
+endif
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=EM-RETRO.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/EM-RETRO.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=em-retro.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/em-retro.tar
